fec116b2bc008e9f4d2428d97ce5d1c8af262a74
Шабанян Александр Эрнестович <ashabanyan@phoenixit.ru>
2024-03-12 23:24:27 +0300

    fix: check style

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidDate.java
@@ -13,8 +13,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Constraint(validatedBy = DateValidator.class)
 @Documented
-public @interface ValidDate{
-    String message() default "{message.key}";
+public @interface ValidDate {
+    String message() default "Валидация провалена";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
     String targetDate() default "";
