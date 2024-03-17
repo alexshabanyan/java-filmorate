4acd137b17e171f7795987fc9d55ae7c62679b59
Шабанян Александр Эрнестович <ashabanyan@phoenixit.ru>
2024-03-18 00:34:02 +0300

    fix: check style v1

--- src/main/java/ru/yandex/practicum/filmorate/exception/ItemNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ItemNotFoundException.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ItemNotFoundException extends RuntimeException {
     private final Long id;
+
     public ItemNotFoundException(Long id) {
         this.id = id;
     }
